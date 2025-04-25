6df2b82fc315ef255614804f320bb7db0cc7d202
iagoiago <iagolaferl1401@gmail.com>
2025-04-25 19:29:13 -0300

    Update todo-list

---
+++ todo-list/script.js
@@ -0,0 +1,5 @@
+const newTaskButton = document.querySelector('.new-task-button');
+const deleteButton = document.querySelector('.delete-button');
+const saveButton = document.querySelector('.save-button');
+
+console.log(newTaskButton, deleteButton, saveButton);
