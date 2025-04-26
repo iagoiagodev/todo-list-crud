5949901c5611adc8f5ce00462618bec1458c243b
iagoiago <iagolaferl1401@gmail.com>
2025-04-26 00:00:55 -0300

    test

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,12 +1,3 @@
-import { createRoot } from 'react-dom/client';
-
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
-
 const newTaskButton = document.querySelector('.new-task-button');
 const deleteButton = document.querySelector('.delete-button');
 const saveButton = document.querySelector('.save-button');
