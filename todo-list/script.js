4a9eec81e2e49ef9b40936ec3da603460003c65e
iagoiago <iagolaferl1401@gmail.com>
2025-04-25 21:15:46 -0300

    test

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,5 +1,5 @@
 const newTaskButton = document.querySelector('.new-task-button');
 const deleteButton = document.querySelector('.delete-button');
 const saveButton = document.querySelector('.save-button');
-
+const accordion = document.querySelector('#accordionExample');
 console.log(newTaskButton, deleteButton, saveButton);
