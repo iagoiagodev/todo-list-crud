752fa832bec2cb8449628f7234fd3348bd13fdf1
iagoiago <iagolaferl1401@gmail.com>
2025-04-29 17:08:49 -0300

    Update backend

--- todo-list/backend/database.js
+++ todo-list/backend/database.js
@@ -4,8 +4,11 @@ const db = new sqlite3.Database('todos.db');
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS todos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    done INTEGER DEFAULT 0
+    taskName TEXT NOT NULL,
+    startDate TEXT NOT NULL,
+    endDate TEXT NOT NULL,
+    estimatedCost REAL NOT NULL CHECK(estimatedCost >= 0),
+    taskStatus TEXT NOT NULL CHECK(taskStatus IN ('Concluída', 'Em Andamento', 'Pendente'))
   )`);
 });
 
